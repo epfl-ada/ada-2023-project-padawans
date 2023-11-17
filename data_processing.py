3206afe1962b73d0b87d2860fdd7165190076e08
OuldBay-Abderrahmane <ouldbay.abderrahmane@gmail.com>
2023-11-17 15:28:43 +0100

    correct

--- data_processing.py
+++ data_processing.py
@@ -5,30 +5,46 @@ import spacy
 
 
 def processing():
+    df_plot_summaries = pd.read_csv('MovieSummaries/plot_summaries.txt', sep='\t', names = ['WikipediaMovieID', 'PlotSummaries'], index_col = ['WikipediaMovieID'])
+    """    df_character = pd.read_csv("MovieSummaries/character.metadata.tsv", sep='\t', names = ['WikipediaMovieID', 'FreebaseMovieID', 'MovieReleaseDate', 'CharacterName', 
+                                                                        'ActorDateOfBirth', 'ActorGender', 'ActorHeight', 'ActorEthnicity', 
+                                                                        'ActorName', 'ActorAgeAtMovieRelease','FreebaseCharacter', 
+                                                                        'FreebaseCharacterID', 'FreebaseActorID'], index_col = ['WikipediaMovieID'])"""
+    df_movies = pd.read_csv("MovieSummaries/movie.metadata.tsv", sep='\t', header = None, names = ['WikipediaMovieID', 'FreebaseMovieID', 'MovieName', 
+                                                                                        'MovieReleaseDate', 'MovieBoxOfficeRevenue',
+                                                                                        'MovieRuntime', 'MovieLanguages', 'MovieCountries',
+                                                                                        'MovieGenres'], index_col = ['WikipediaMovieID'])
+
+
+    df_movies['MovieReleaseDate'] = pd.to_datetime(df_movies['MovieReleaseDate'], format='%Y-%m-%d', errors='coerce').dt.year.astype('Int64')
+    df_movies = pd.merge(df_movies, df_plot_summaries, how='left', on ='WikipediaMovieID')
+
+
     # Load the CSV files into pandas DataFrames
     rating = pd.read_csv('data/ratings.csv')
     links = pd.read_csv('data/links.csv')
-    movies = pd.read_csv('data/movies_metadata.csv')
-    summaries = pd.read_csv('data/plot_summaries.txt',header=None, names=['Wikipedia_ID','Summary'])
-    nlp = spacy.load("en_core_web_sm")
+    movies = pd.read_csv('data/movies_metadata.csv', low_memory=False)
+
+
+    # Load spaCy model with disabled components for efficiency
+    nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+
+    # Pre-compile the list of stop words for faster checking
+    stop_words = nlp.Defaults.stop_words
+
+    # Function to check if token should be included
+    def is_token_allowed(token):
+        return not token.is_punct and token.pos_ in ["ADJ", "PROPN", "NOUN"] and token.text.lower() not in stop_words
+
+    # Function to preprocess token
+    def preprocess_token(token):
+        return token.lemma_.strip().lower()
 
     def process_text(summary):
-        def is_token_allowed(token):
-            return bool(
-            token
-            and str(token).strip()
-            and token.pos_ in ["ADJ" ,"PROPN","NOUN"]
-            and not token.is_stop
-            and not token.is_punct
-            )
-    
-        def preprocess_token(token):
-            return token.lemma_.strip().lower()
-    
-        # Process the text with spaCy
+        # Ensure the text is treated as a writable string
         doc = nlp(summary)
+        # Convert the processed tokens to strings to avoid any complex types
         return [preprocess_token(token) for token in doc if is_token_allowed(token)]
-    
 
     # Group by 'id' and calculate the average rating for each group
     average_ratings = rating.groupby('movieId')['rating'].agg(np.nanmean).reset_index()
@@ -41,21 +57,33 @@ def processing():
     merged_df.rename(columns={'imdbId': 'imdb_id'}, inplace=True)
     merged_df.imdb_id = merged_df['imdb_id']
     # Change types
+
     merged_df['imdb_id'] = merged_df['imdb_id'].astype(str)
     movies['imdb_id'] = movies['imdb_id'].apply(strip).astype(str)
     movies = movies.astype(str)
 
     merged_df = pd.merge(merged_df, movies, on='imdb_id', how='inner')
 
-    ### 20 minutes long 
-    #summaries['tokens']= sm['Summary'].apply(lambda x : process_text(x))
-    
-    #pd.merge(merged_df, summaries, on='Wikipedia_ID', how='left')
-   
+    #df_plot_summaries['tokens']= df_plot_summaries['Summary'].apply(process_text)
 
+    #pd.merge(merged_df, df_plot_summaries, on='Wikipedia_ID', how='left')
+
+    merged_df.release_date = pd.to_datetime(merged_df.release_date, errors='coerce')
+
+    # Now extract the year
+    merged_df.release_date = merged_df.release_date.dt.year.astype("Int64")
+    merged_df.release_date
+
+
+    columns = ['FreebaseMovieID', 'original_title', 'release_date',
+        'MovieBoxOfficeRevenue', 'MovieRuntime', 'MovieLanguages',
+        'MovieCountries', 'MovieGenres', 'PlotSummaries']
+    df_movies.columns = columns
+
+
+    merged_df = pd.merge(merged_df, df_movies, how= 'outer', on =['original_title', 'release_date'])
     # Save the merged DataFrame to a new CSV file
     merged_df.to_csv('data/merged_file.csv', index=False)
-
 
 
 def main():
